909c22a0cb840b2094ea6a424f4d31ee23205a04
Rafael Teno Dias <rfteno@gmail.com>
2024-04-25 00:28:10 -0400

    Adicionado Helpers para o Designer; Finalizado menu relatório.

---
+++ src/FastReport.Designer/Commom/FrDesignerControlHelper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Reactive;
+using FastReport.Design.StandardDesigner;
+using FastReport.Designer.Extensions;
+using ReactiveUI;
+
+namespace FastReport.Designer.Commom;
+
+public sealed class FrDesignerControlHelper
+{
+    #region Constructors
+
+    public FrDesignerControlHelper(DesignerControl designerControl)
+    {
+        designerControl.MainMenu.miReport.DropDownItems[0].SetChecked(true);
+        ReportTitle = designerControl.MainMenu.miReport.DropDownItems[0].ToCheckedObservable();
+        ReportTitleCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[0].ToggleChecked();
+        });
+        
+        designerControl.MainMenu.miReport.DropDownItems[1].SetChecked(false);
+        ReportSummary = designerControl.MainMenu.miReport.DropDownItems[1].ToCheckedObservable();
+        ReportSummaryCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[1].ToggleChecked();
+        });
+        
+        designerControl.MainMenu.miReport.DropDownItems[2].SetChecked(true);
+        ReportHeader = designerControl.MainMenu.miReport.DropDownItems[2].ToCheckedObservable();
+        ReportHeaderCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[2].ToggleChecked();
+        });
+        
+        designerControl.MainMenu.miReport.DropDownItems[3].SetChecked(true);
+        ReportFooter = designerControl.MainMenu.miReport.DropDownItems[3].ToCheckedObservable();
+        ReportFooterCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[3].ToggleChecked();
+        });
+        
+        designerControl.MainMenu.miReport.DropDownItems[4].SetChecked(false);
+        HeaderColumn = designerControl.MainMenu.miReport.DropDownItems[3].ToCheckedObservable();
+        HeaderColumnCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[4].ToggleChecked();
+        });
+
+        
+        designerControl.MainMenu.miReport.DropDownItems[5].SetChecked(false);
+        FooterColumn = designerControl.MainMenu.miReport.DropDownItems[5].ToCheckedObservable();
+        FooterColumnCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[5].ToggleChecked();
+        });
+
+        
+        designerControl.MainMenu.miReport.DropDownItems[6].SetChecked(false);
+        OverlayColumn = designerControl.MainMenu.miReport.DropDownItems[6].ToCheckedObservable();
+        OverlayColumnCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[6].ToggleChecked();
+        });
+
+
+        ConfigurarBandasCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[8].PerformClick();
+        });
+        
+        AssistenteGruposCommand = ReactiveCommand.Create(() =>
+        {
+            designerControl.MainMenu.miReport.DropDownItems[9].PerformClick();
+        });
+    }
+
+    #endregion Constructors
+
+    #region Properties
+
+    public IObservable<bool> ReportTitle { get; }
+    
+    public IObservable<bool> ReportSummary { get; }
+    
+    public IObservable<bool> ReportHeader { get; }
+    
+    public IObservable<bool> ReportFooter { get; }
+    
+    public IObservable<bool> HeaderColumn { get; }
+    
+    public IObservable<bool> FooterColumn { get; }
+    
+    public IObservable<bool> OverlayColumn { get; }
+    
+    public ReactiveCommand<Unit, Unit> ReportTitleCommand { get; }
+    
+    public ReactiveCommand<Unit, Unit> ReportSummaryCommand { get; }
+    
+    public ReactiveCommand<Unit, Unit> ReportHeaderCommand { get; }
+    
+    public ReactiveCommand<Unit, Unit> ReportFooterCommand { get; }
+    
+    public ReactiveCommand<Unit, Unit> HeaderColumnCommand { get; }
+    
+    public ReactiveCommand<Unit, Unit> FooterColumnCommand { get; }
+    
+    public ReactiveCommand<Unit, Unit> OverlayColumnCommand { get; }
+    
+    public ReactiveCommand<Unit, Unit> ConfigurarBandasCommand { get; }
+    
+    public ReactiveCommand<Unit, Unit> AssistenteGruposCommand { get; }
+
+    #endregion Properties
+}
---
+++ src/FastReport.Designer/Extensions/DesignerControlExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using FastReport.Design;
+using FastReport.Designer.Commom;
+using ReactiveUI;
+
+namespace FastReport.Designer.Extensions;
+
+public static class DesignerControlExtensions
+{
+    public static FrDesignerControlHelper GetHelper(this AvaloniaDesignerControl control)
+    {
+        return new FrDesignerControlHelper(control.InnerDesigner);
+    }
+
+    public static IObservable<bool> ToCheckedObservable(this ToolStripItem item)
+    {
+        var itemMenu = (ToolStripMenuItem) item;
+        var field = typeof(ToolStripMenuItem).GetField("menuItem", BindingFlags.Instance | BindingFlags.NonPublic);
+        if (field == null) throw new Exception();
+        
+        var internalItem = (CustomControls.ToolStripMenuItem?)field.GetValue(itemMenu);
+        return internalItem.WhenAnyValue(x => x.IsChecked);
+    }
+    
+    public static void SetChecked(this ToolStripItem item, bool check)
+    {
+        var itemMenu = (ToolStripMenuItem) item;
+        itemMenu.Checked = check;
+    }
+    
+    public static bool GetChecked(this ToolStripItem item)
+    {
+        var itemMenu = (ToolStripMenuItem) item;
+        return itemMenu.Checked;
+    }
+    
+    public static void ToggleChecked(this ToolStripItem item)
+    {
+        var itemMenu = (ToolStripMenuItem) item;
+        itemMenu.Checked = !itemMenu.Checked;
+        itemMenu.PerformClick();
+    }
+}
